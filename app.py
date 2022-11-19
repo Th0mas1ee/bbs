b459e72e96b4200bf1a6dc794e8a7734fa716862
Th0mas1ee <Thomass1ee@163.com>
2022-11-20 00:51:05 +0800

    app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from routes.todo import main as todo_routes
+
+
+app = Flask(__name__)
+app.secret_key = 'random string'
+app.register_blueprint(todo_routes, url_prefix='/todo')
+
+
+if __name__ == '__main__':
+    config = dict(
+        debug=True,
+        host='0.0.0.0',
+        port=2000,
+    )
+    app.run(**config)
