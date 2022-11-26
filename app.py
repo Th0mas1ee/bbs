c88ef82a55cb9defc187fd1f0be5db8a14f7a25a
Th0mas1ee <Thomas1ee@163.com>
2022-11-26 22:08:54 +0800

    add module mail

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.index import main as index_routes
 from routes.topic import main as topic_routes
 from routes.reply import main as reply_routes
 from routes.board import main as board_routes
+from routes.mail import main as mail_routes
 
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ app.register_blueprint(index_routes)
 app.register_blueprint(topic_routes, url_prefix='/topic')
 app.register_blueprint(reply_routes, url_prefix='/reply')
 app.register_blueprint(board_routes, url_prefix='/board')
+app.register_blueprint(mail_routes, url_prefix='/mail')
 # app.register_blueprint(todo_routes, url_prefix='/todo')
 
 
