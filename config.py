2b272226ede4e386e84845b9d9785ba79c3b3031
Th0mas1ee <Thomas1ee@163.com>
2022-11-26 21:28:04 +0800

    add function uploading

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 secret_key = 'random string'
+accpet_user_file_type = ['jpg', 'gif', 'png']
+user_file_directory = r'E:\Photography\星云图'
--- routes/index.py
+++ routes/index.py
@@ -5,11 +5,13 @@ from flask import (
     redirect,
     session,
     Blueprint,
+    send_from_directory,
 )
-
+from werkzeug.utils import secure_filename
 from models.user import User
 from routes import current_user
-
+from config import user_file_directory
+import os
 from utils import log
 
 main = Blueprint('index', __name__)
@@ -47,3 +49,32 @@ def profile():
         return redirect(url_for('.index'))
     else:
         return render_template('profile.html', user=u)
+
+
+def allow_file(filename: str):
+    suffix = filename.split('.')[-1]
+    from config import accpet_user_file_type
+    return suffix in accpet_user_file_type
+
+
+@main.route('/add_img', methods=['POST'])
+def add_img():
+    u = current_user()
+    if u is None:
+        return redirect(url_for('.profile'))
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if allow_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(user_file_directory, filename))
+        u.user_image = filename
+        u.save()
+    return redirect(url_for('.profile'))
+
+
+@main.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory(user_file_directory, filename)
